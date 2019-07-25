aaccbfee02c2b33ad1fe2d3d0c77a9b25657f3a5
abhiraj <abhiraj.tomar@juspay.in>
2019-07-25 16:43:43 +0530

    added context menu for length of selected text

--- eventPage.js
+++ eventPage.js
@@ -4,15 +4,22 @@ var contextMenuItem = {
     "contexts": ["selection"]
 };
 
+var contextMenuItem1 = {
+    "id": "getLength",
+    "title": "get Length",
+    "contexts": ["selection"]
+};
+
 chrome.contextMenus.create(contextMenuItem);
+chrome.contextMenus.create(contextMenuItem1);
 
 chrome.contextMenus.onClicked.addListener(function (clickData) {
     if (clickData.menuItemId == "searchInKeeper" && clickData.selectionText) {
-        chrome.storage.sync.set({'keyword': clickData.selectionText }, function () {
+        chrome.storage.sync.set({ 'keyword': clickData.selectionText }, function () {
             chrome.tabs.create({
                 url: chrome.extension.getURL('popup.html'),
                 active: false
-            }, function(tab) {
+            }, function (tab) {
                 // After the tab has been created, open a window to inject the tab
                 chrome.windows.create({
                     tabId: tab.id,
@@ -22,5 +29,8 @@ chrome.contextMenus.onClicked.addListener(function (clickData) {
                 });
             });
         });
+    }
+    if (clickData.menuItemId == "getLength" && clickData.selectionText) {
+        alert((clickData.selectionText).length);
     }
 });
